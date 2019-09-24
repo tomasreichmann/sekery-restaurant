ec8c2d441272e4c68dcde2fa369a526116a1468a
tomasreichmann <tomasreichmann@gmail.com>
2019-09-24 22:22:12 +0200

    production env fix

--- next.config.js
+++ next.config.js
@@ -4,16 +4,15 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   webpack(config) {
     config.plugins = config.plugins || []
-
-    config.plugins = [
-      ...config.plugins,
-
+    if (!process.env.NODE_ENV === "production") {
       // Read the .env file
-      new Dotenv({
-        path: path.join(__dirname, '.env'),
-        systemvars: true
-      })
-    ];
+      config.plugins.push(
+        new Dotenv({
+          path: path.join(__dirname, '.env'),
+          systemvars: true
+        })
+      )
+    }
 
     config.module.rules.push({
       test: /\.svg$/,
